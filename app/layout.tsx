7e6c9f3f2dbf4e6fcb63df3797eda4b3c6500eb3
0xgokkull <gokkull04@gmail.com>
2026-03-29 21:13:04 +0530

    refactor: reduce top gap below header (py-8 to py-4) and adjust content padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,12 +32,12 @@ export default function RootLayout({
             
             <div 
               className={`flex-1 flex flex-col min-w-0 transition-all duration-500 ease-[cubic-bezier(0.16,1,0.3,1)] overflow-hidden ${
-                isLanding ? 'pl-0' : (isCollapsed ? 'pl-[72px]' : 'pl-[240px]')
+                isLanding ? 'pl-0' : (isCollapsed ? 'pl-[80px]' : 'pl-[280px]')
               }`}
             >
               {!isLanding && <TopHeader isCollapsed={isCollapsed} />}
               <main className="flex-1 overflow-y-auto">
-                <div className={`${isLanding ? '' : 'wide-container py-8'}`}>
+                <div className={`${isLanding ? '' : 'wide-container py-4'}`}>
                   {children}
                 </div>
               </main>
